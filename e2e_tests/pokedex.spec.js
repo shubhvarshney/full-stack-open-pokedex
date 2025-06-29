7a31fb2569f04da6e9313c324846ddcd04e601b7
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-06-29 17:19:30 -0500

    fixed lint errors

--- e2e_tests/pokedex.spec.js
+++ e2e_tests/pokedex.spec.js
@@ -3,7 +3,6 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 describe('Pokedex', () => {
   beforeEach(async ({ page }) => {
     await page.goto('/')
-    console.log('Current URL:', page.url());
   })
   test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
@@ -11,7 +10,6 @@ describe('Pokedex', () => {
   })
   test('pokemon page can be navigated to', async ({ page }) => {
     await page.getByText('ivysaur').click()
-    console.log('Current URL:', page.url());
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
