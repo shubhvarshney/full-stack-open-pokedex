bfcf1b7470d90cfec8c63ea4f724794a630b1520
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-06-30 14:01:26 -0500

    Added health check endpoint

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
 app.get('/version', (req, res) => {
   res.send('2')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
